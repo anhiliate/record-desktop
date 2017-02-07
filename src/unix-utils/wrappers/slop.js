1c73280e236e8870ee6becb37df95a530199179e
ewnd9 <ewndnine@gmail.com>
2017-02-07 16:45:11 +0800

    add ignoring keyboard flag for slop, fixes #25

--- src/unix-utils/wrappers/slop.js
+++ src/unix-utils/wrappers/slop.js
@@ -1,7 +1,7 @@
 import { exec } from '../utils';
 
 export default function slop() {
-  return exec(`slop`).then(res => {
+  return exec(`slop --nokeyboard`).then(res => {
     const [x, y, width, height] = res.split('\n').map(_ => _.split('=')[1]);
     return { width, height, x, y };
   });
